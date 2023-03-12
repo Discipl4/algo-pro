bf8cf0e6d7d4e7f939fc10e4c631ebe48c1227a7
Discipl4 <gghssa1@gmail.com>
2023-03-13 01:18:47 +0700

    ┬┴┬┴┤(･_├┬┴┬┴

---
+++ Pertemuan 2/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+int tambah(int a, int b);
+int kurang(int a, int b);
+int kali(int a, int b);
+int total(int arr[], int index);
+int rata(int arr[], int index);
+
+int main()
+{
+    int a = 8;
+    int b = 5;
+    int ray[5] = {1, 3, 5, 7, 9};
+
+    cout << "Pertambahan = " << tambah(a, b) << endl;
+    cout << "Pengurangan = " << kurang(a, b) << endl;
+    cout << "Perkalian = " << kali(a, b) << endl;
+    cout << "Total = " << total(ray, 5) << endl;
+    cout << "Rata-rata = " << rata(ray, 5) << endl;
+
+    return 0;
+}
+
+int tambah(int a, int b)
+{
+    return a + b;
+}
+
+int kurang(int a, int b)
+{
+    return a - b;
+}
+
+int kali(int a, int b)
+{
+    return a * b;
+}
+
+int total(int arr[], int index)
+{
+    int temp = 0;
+
+    for (int i = 0; i < index; i++)
+    {
+        temp += arr[i];
+    }
+
+    return temp;
+}
+
+int rata(int arr[], int index)
+{
+    int temp = 0;
+
+    for (int i = 0; i < index; i++)
+    {
+        temp += arr[i];
+    }
+
+    return temp / index;
+}
